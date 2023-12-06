3168e97c9cb9f96de6de14bde8c0c405eedfdce5
fsampson30 <fsampson30@gmail.com>
2023-12-06 07:24:18 -0300

    Initial commit

---
+++ src/test/java/com/sampson/aulaspringjpa/AulaSpringJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sampson.aulaspringjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaSpringJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
