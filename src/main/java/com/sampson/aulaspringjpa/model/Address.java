c41ffa67c7fd6ad6f4262f1e7e090299033490b6
fsampson30 <fsampson30@gmail.com>
2023-12-07 17:40:37 -0300

    Created Entity class Address

---
+++ src/main/java/com/sampson/aulaspringjpa/model/Address.java
@@ -0,0 +1,58 @@
+package com.sampson.aulaspringjpa.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String street;
+    private String city;
+
+    public Address(Integer id, String street, String city) {
+        this.id = id;
+        this.street = street;
+        this.city = city;
+    }
+
+    public Address() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "id=" + id +
+                ", street='" + street + '\'' +
+                ", city='" + city + '\'' +
+                '}';
+    }
+}
