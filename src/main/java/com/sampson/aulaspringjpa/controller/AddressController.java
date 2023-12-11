9a01418ea876cdc94fcd538a41a007e82600f6a1
fsampson30 <fsampson30@gmail.com>
2023-12-11 19:08:44 -0300

    Organized imports.

--- src/main/java/com/sampson/aulaspringjpa/controller/AddressController.java
+++ src/main/java/com/sampson/aulaspringjpa/controller/AddressController.java
@@ -2,9 +2,7 @@ package com.sampson.aulaspringjpa.controller;
 
 import com.sampson.aulaspringjpa.model.Address;
 import com.sampson.aulaspringjpa.service.AddressService;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
--- src/main/java/com/sampson/aulaspringjpa/controller/UserController.java
+++ src/main/java/com/sampson/aulaspringjpa/controller/UserController.java
@@ -2,7 +2,6 @@ package com.sampson.aulaspringjpa.controller;
 
 import com.sampson.aulaspringjpa.model.User;
 import com.sampson.aulaspringjpa.service.UserService;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
